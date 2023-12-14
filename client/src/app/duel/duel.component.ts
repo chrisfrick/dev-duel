5d06266532029e5ba015f79be704b18e48d13f8d
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-12-14 14:45:34 -0500

    visually distinguish winner

--- client/src/app/duel/duel.component.ts
+++ client/src/app/duel/duel.component.ts
@@ -11,36 +11,43 @@ export class DuelComponent implements OnInit {
   usernameOne: string = '';
   usernameTwo: string = '';
 
-  currentUserOne: User | null = {
-    username: 'gaearon',
-    name: 'Dan Abramov',
-    location: 'London, UK',
-    bio: 'Working on @reactjs. Co-author of Redux and Create React App. Building tools for humans.',
-    avatar_url: 'https://avatars0.githubusercontent.com/u/810438?v=4',
-    titles: ['Jack of all Trades', 'Mr. Popular'],
-    'favorite-language': 'JavaScript',
-    'public-repos': 227,
-    'total-stars': 120580,
-    'highest-starred': 45820,
-    'perfect-repos': 2,
-    followers: 33015,
-    following: 171,
-  };
-  currntUserTwo: User | null = {
-    username: 'gaearon',
-    name: 'dan',
-    location: '',
-    bio: '',
-    avatar_url: 'https://avatars.githubusercontent.com/u/810438?v=4',
-    titles: ['Forker', 'Mr. Popular'],
-    'favorite-language': 'JavaScript',
-    'public-repos': 268,
-    'total-stars': 1524,
-    'highest-starred': 1086,
-    'perfect-repos': 3,
-    followers: 85296,
-    following: 172,
-  };
+  currentUserOne: User | null = null;
+
+  // test data
+  // {
+  //   username: 'gaearon',
+  //   name: 'Dan Abramov',
+  //   location: 'London, UK',
+  //   bio: 'Working on @reactjs. Co-author of Redux and Create React App. Building tools for humans.',
+  //   avatar_url: 'https://avatars0.githubusercontent.com/u/810438?v=4',
+  //   titles: ['Jack of all Trades', 'Mr. Popular'],
+  //   'favorite-language': 'JavaScript',
+  //   'public-repos': 227,
+  //   'total-stars': 120580,
+  //   'highest-starred': 45820,
+  //   'perfect-repos': 2,
+  //   followers: 33015,
+  //   following: 171,
+  // };
+
+  currentUserTwo: User | null = null;
+
+  // test data
+  // {
+  //   username: 'gaearon',
+  //   name: 'dan',
+  //   location: '',
+  //   bio: '',
+  //   avatar_url: 'https://avatars.githubusercontent.com/u/810438?v=4',
+  //   titles: ['Forker', 'Mr. Popular'],
+  //   'favorite-language': 'JavaScript',
+  //   'public-repos': 268,
+  //   'total-stars': 1524,
+  //   'highest-starred': 1086,
+  //   'perfect-repos': 3,
+  //   followers: 85296,
+  //   following: 172,
+  // };
 
   errorMessage: string = '';
 
@@ -79,11 +86,11 @@ export class DuelComponent implements OnInit {
     this.userService.duelUsers(this.usernameOne, this.usernameTwo).subscribe({
       next: (users) => {
         this.currentUserOne = users[0];
-        this.currntUserTwo = users[1];
-        if (this.currentUserOne && this.currntUserTwo) {
+        this.currentUserTwo = users[1];
+        if (this.currentUserOne && this.currentUserTwo) {
           this.winner = this.chooseWinner(
             this.currentUserOne,
-            this.currntUserTwo
+            this.currentUserTwo
           );
         }
       },
--- client/src/app/inspect/inspect.component.ts
+++ client/src/app/inspect/inspect.component.ts
@@ -10,21 +10,24 @@ import { UserService } from 'src/user.service';
 export class InspectComponent implements OnInit {
   username: string = '';
 
-  currentUser: User | null = {
-    username: 'gaearon',
-    name: 'Dan Abramov',
-    location: 'London, UK',
-    bio: 'Working on @reactjs. Co-author of Redux and Create React App. Building tools for humans.',
-    avatar_url: 'https://avatars0.githubusercontent.com/u/810438?v=4',
-    titles: ['Jack of all Trades', 'Mr. Popular'],
-    'favorite-language': 'JavaScript',
-    'public-repos': 227,
-    'total-stars': 120580,
-    'highest-starred': 45820,
-    'perfect-repos': 2,
-    followers: 33015,
-    following: 171,
-  };
+  currentUser: User | null = null;
+
+  // test data
+  // {
+  //   username: 'gaearon',
+  //   name: 'Dan Abramov',
+  //   location: 'London, UK',
+  //   bio: 'Working on @reactjs. Co-author of Redux and Create React App. Building tools for humans.',
+  //   avatar_url: 'https://avatars0.githubusercontent.com/u/810438?v=4',
+  //   titles: ['Jack of all Trades', 'Mr. Popular'],
+  //   'favorite-language': 'JavaScript',
+  //   'public-repos': 227,
+  //   'total-stars': 120580,
+  //   'highest-starred': 45820,
+  //   'perfect-repos': 2,
+  //   followers: 33015,
+  //   following: 171,
+  // };
 
   errorMessage: string = '';
 
