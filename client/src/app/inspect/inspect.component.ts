632dfe8a4755b7f725e6ed9ac2474d9f211b4f2b
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-12-13 21:11:40 -0500

    create user field on Inspect component

--- client/src/app/inspect/inspect.component.ts
+++ client/src/app/inspect/inspect.component.ts
@@ -1,29 +1,28 @@
 import { Component, Input, OnInit } from '@angular/core';
+import User from 'src/types/User';
 import { UserService } from 'src/user.service';
 
 @Component({
   selector: 'app-inspect',
   templateUrl: './inspect.component.html',
-  styleUrls: ['./inspect.component.css']
+  styleUrls: ['./inspect.component.css'],
 })
 export class InspectComponent implements OnInit {
+  username: string = '';
 
-  username: string = ""
+  user: User | null = null;
 
+  constructor(private userService: UserService) {}
 
-  constructor(private userService: UserService) { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   receiveUsername(valueEmitted: string) {
     this.username = valueEmitted;
   }
 
   onSubmit() {
-    this.userService.inspectUser(this.username);
+    this.userService
+      .inspectUser(this.username)
+      .subscribe((user) => (this.user = user));
   }
-
-
-
 }
--- client/src/user.service.ts
+++ client/src/user.service.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import User from './types/User';
 
 const inspectUserUrl = 'http://localhost:3000/api/user/';
 const duelUsersUrl = 'http://localhost:3000/api/users?';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  async inspectUser(username = 'andrew') {
-    let data = await this.http.get(inspectUserUrl + username).toPromise();
+  inspectUser(username = 'andrew') {
+    let data = this.http.get<User>(inspectUserUrl + username);
     console.log(data);
     return data;
   }
 
   async duelUsers(user1 = 'fabpot', user2 = 'andrew') {
-    let data = await this.http.get(duelUsersUrl + `username=${user1}&username=${user2}`).toPromise();
+    let data = await this.http
+      .get(duelUsersUrl + `username=${user1}&username=${user2}`)
+      .toPromise();
     console.log(data);
     return data;
   }
-
 }
